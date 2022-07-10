bb09d04c469d1fea8dc91f7d2d091c02aa479c3d
Niko Shin <nshin17@gmail.com>
2022-07-09 22:14:26 -0700

    checkpoint - working on JS to check for duplicate user input.

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
 const header = document.querySelector('.header');
+const main = document.querySelector('.main');
 
 
-
-
-
-
+// MAIN BUTTON "ADD BOOK" MODAL:
 // Get the modal
 const modal = document.getElementById("myModal");
 
@@ -18,14 +16,14 @@ const span = document.getElementsByClassName("close")[0];
 btn.addEventListener('click', () => {
     modal.style.display = 'block';
     header.classList.add('is-blurred'); // Add blur effect.
-
+    main.classList.add('is-blurred');
 })
-
 
 // When the user clicks on <span> (x), close the modal
 span.addEventListener('click', () => {
     modal.style.display = 'none';
     header.classList.remove('is-blurred'); // Remove blur effect.
+    main.classList.remove('is-blurred');
 })
 
 // When the user clicks anywhere outside of the modal, close it
@@ -33,6 +31,7 @@ window.addEventListener('click', (event) => {
     if (event.target == modal) {
         modal.style.display = 'none';
         header.classList.remove('is-blurred'); // Remove blur effect.
+        main.classList.remove('is-blurred');
     }
 })
 
@@ -45,58 +44,89 @@ window.addEventListener('click', (event) => {
 
 
 
+const title = document.getElementById('title');
+const author = document.getElementById('author');
+const pages = document.getElementById('pages');
+const genre = document.getElementById('genre');
+const notes = document.getElementById('notes');
+const submitBtn = document.getElementById('btn');
+const myBtn = document.getElementById('myBtn');
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-const form = document.getElementById('form');
-const button = document.querySelector('button');
-
-let myLibrary = ['The Hobbit', 'Lord of the Rings', 'Harry Potter'];
-
-function Book() {
+class Book {
+    constructor(title, author, pages, genre) {
+    this.title = document.getElementById('title'),
+    this.author = document.getElementById('author'),
+    this.pages = document.getElementById('pages'),
+    this.genre = document.getElementById('genre')
+    // this.notes = notes
+    };
 };
 
-function addBookToLibrary() {
+
+
+
+let myLibrary = [];
+console.log(myLibrary);
+
+class BookInfo {
+    constructor(title, author, pages, genre) {
+        this.title = title,
+        this.author = author,
+        this.pages = pages,
+        this.genre = genre
+    };
 };
 
-// Display book in myLibrary:
-const showBook = function() {
-    myLibrary.forEach(book => {
-        console.log(book);
+// Add new book to library array:
+const addBook = function() {
+    submitBtn.addEventListener('click', (e) => {
+        e.preventDefault();
+        const newBook = new BookInfo(title.value, author.value, pages.value, genre.value);
+        if (myLibrary.length > 0) {
+            // If duplicate isn't found:
+            myLibrary.forEach(book => {
+                if (newBook.title != book.title) {
+                    myLibrary.push(newBook);
+                    alert(`${title.value} has been added to your library.`);
+                    document.querySelector('form').reset(); // clear form after submit.
+                } else {
+                    alert(`${title.value} already exists in your library!`);
+                    document.querySelector('form').reset(); // clear form after submit.
+                };
+            })
+        } else {
+            myLibrary.push(newBook); // Add first book to library.
+            alert(`${title.value} has been added to your library!`);
+            document.querySelector('form').reset(); // clear form after submit.
+        }
     });
 };
 
-// window.onload = function() {
-//     document.getElementById('form').style.display = 'block';
-// };
+addBook();
 
 
-// button.addEventListener('click', () => {
-//     if (form.style.display === 'none') {
-//         form.style.display = 'block';
-//     } else {
-//         form.style.display = 'none';
-//     }
-// });
+
+
+
+
+
+
+// If user input title already exists in the library, error occurs:
+// iterate through each title element in library.
+// check if the user input title matches the title in library.
+// if so, then error message occurs:
+const checkDuplicate = function() {
+    myLibrary.forEach(book => {
+        if (title.value == book.title) {
+            return true; // Return true if a duplicate exists.
+        };
+        return false;
+    });
+}
+
+
+
+
+
