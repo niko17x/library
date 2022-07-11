499c53e1b2f17957f50574d3421028f4ffa0483c
Niko Shin <nshin17@gmail.com>
2022-07-11 16:06:00 -0700

    Completed createBookCards() function that now creates a book card by iterating through each object in the myLibrary array

--- script.js
+++ script.js
@@ -52,17 +52,25 @@ const notes = document.getElementById('notes');
 const submitBtn = document.getElementById('btn');
 const myBtn = document.getElementById('myBtn');
 
-class Book {
-    constructor(title, author, pages, genre) {
-    this.title = document.getElementById('title'),
-    this.author = document.getElementById('author'),
-    this.pages = document.getElementById('pages'),
-    this.genre = document.getElementById('genre')
-    // this.notes = notes
-    };
-};
+// class Book {
+//     constructor(title, author, pages, genre) {
+//     this.title = document.getElementById('title'),
+//     this.author = document.getElementById('author'),
+//     this.pages = document.getElementById('pages'),
+//     this.genre = document.getElementById('genre')
+//     // this.notes = notes
+//     };
+// };
 
-let myLibrary = [];
+let myLibrary = [
+    {
+        title: 'Harry Potter',
+        author: 'JK Rowling',
+        pages: 300,
+        genre: 'Magic',
+        notes: 'This is an example notes blah blah blah.'
+    }
+];
 
 class BookInfo {
     constructor(title, author, pages, genre) {
@@ -109,9 +117,78 @@ function findDuplicate(book) {
 
 
 
+const bookLibrary = document.querySelector('.book-library');
+const divBooks = document.createElement('div');
+const divBookHeader = document.createElement('div');
+const spanClose = document.createElement('span');
+const h2Title = document.createElement('h2');
+const divBookBody = document.createElement('div');
+const newP1 = document.createElement('p');
+const newP2 = document.createElement('p');
+const newP3 = document.createElement('p');
+const newP4 = document.createElement('p');
+const divBookFooter = document.createElement('div');
+const buttonEdit = document.createElement('button');
+const buttonNotRead = document.createElement('button');
+
+// Iterate through library and create a "book card" for each book object inside the array:
+function createBookCards() {    
+    myLibrary.forEach(book => {
+        divBooks.classList.add('books');
+        bookLibrary.appendChild(divBooks);
+    
+        divBookHeader.classList.add('book-header');
+        divBooks.append(divBookHeader);
+        
+        // Book Title:
+        spanClose.classList.add('close');
+        h2Title.classList.add('title');
+        h2Title.textContent = book.title;
+        divBookHeader.append(spanClose, h2Title);
+    
+        divBookBody.classList.add('book-body');
+        divBooks.appendChild(divBookBody);
+    
+        // Author section:
+        newP1.textContent = 'Author: ' + book.author;
+        newP1.setAttribute('id', 'author');
+        divBookBody.appendChild(newP1);
+        
+        // Pages section:
+        newP2.textContent = 'No. of Pages: ' + book.pages;
+        newP2.setAttribute('id', 'no-of-pages');
+        divBookBody.appendChild(newP2);
+        
+        // Genre section:
+        newP3.textContent = 'Genre: ' + book.genre;
+        newP3.setAttribute('id', 'genre');
+        divBookBody.appendChild(newP3);
+    
+        // Notes section:
+        newP4.textContent = 'Notes: ' + book.notes;
+        newP4.setAttribute('id', 'notes');
+        divBookBody.appendChild(newP4);
+    
+        divBookFooter.classList.add('book-footer');
+        divBooks.appendChild(divBookFooter);
+    
+        buttonEdit.textContent = 'Edit';
+        buttonNotRead.textContent = 'Not Read'
+        divBookFooter.appendChild(buttonEdit);
+        divBookFooter.appendChild(buttonNotRead);
+    });
+}
+
+// delete later - only used for testing:
+bookLibrary.addEventListener('click', () => {
+    createBookCards();
+});
 
 
 
 
 
+// for (let i=0; i<myLibrary.length; i++) {
+//     console.log(myLibrary[i].title);
+// }
 
